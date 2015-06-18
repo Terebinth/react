0ba82f3aea5c547e66592ddce4ccaa97fbffa637
wylie <joshua.cullick@gmail.com>
2015-06-18 11:46:47 +0300

    custom build configs ; took forfuckingever

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-/**
- * Copyright 2013-2015, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
+module.exports = require('./build/modules/React');
 
-'use strict';
+// /**
+//  * Copyright 2013-2015, Facebook, Inc.
+//  * All rights reserved.
+//  *
+//  * This source code is licensed under the BSD-style license found in the
+//  * LICENSE file in the root directory of this source tree. An additional grant
+//  * of patent rights can be found in the PATENTS file in the same directory.
+//  */
 
-var jstransform = require('jstransform/simple');
+// 'use strict';
 
-function transformWithDetails(code, opts) {
-  opts = opts || {};
+// var jstransform = require('jstransform/simple');
 
-  // Copy out the values we need and make sure they are compatible with
-  // jstransform options. Always set react:true.
-  var options = {
-    react: true,
-    harmony: opts.harmony,
-    stripTypes: opts.stripTypes,
-    sourceMapInline: opts.sourceMap,
-    sourceFilename: opts.sourceFilename,
-    es6module: opts.es6module,
-    nonStrictEs6module: opts.nonStrictEs6module,
-    target: opts.target,
-  };
+// function transformWithDetails(code, opts) {
+//   opts = opts || {};
 
-  return jstransform.transform(code, options);
-}
+//   // Copy out the values we need and make sure they are compatible with
+//   // jstransform options. Always set react:true.
+//   var options = {
+//     react: true,
+//     harmony: opts.harmony,
+//     stripTypes: opts.stripTypes,
+//     sourceMapInline: opts.sourceMap,
+//     sourceFilename: opts.sourceFilename,
+//     es6module: opts.es6module,
+//     nonStrictEs6module: opts.nonStrictEs6module,
+//     target: opts.target,
+//   };
 
-module.exports = {
-  transform: function(input, options) {
-    return transformWithDetails(input, options).code;
-  },
-  transformWithDetails: transformWithDetails,
-};
+//   return jstransform.transform(code, options);
+// }
+
+// module.exports = {
+//   transform: function(input, options) {
+//     return transformWithDetails(input, options).code;
+//   },
+//   transformWithDetails: transformWithDetails,
+// };
